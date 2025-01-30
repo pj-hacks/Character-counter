6f9f2b61a748dc51bb8bb2c786f9f62ab8cac53c
joseph prince <josephaniekeme1@gmail.com>
2025-01-30 01:30:29 +0100

    add updated the characters reader

--- character_reading.c
+++ character_reading.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int count = 0;
+    int sentence = 0;
     int i = 0;
+    int char1 = 0;
     printf("\nThis program counts the numbers of characters in a string\n");
 
     while((i = getchar()) != EOF && i != '\n'){
 	if(i == ' '){
-	    count++;   
+	    sentence++;   
 	}
+	char1++;
     }
     if(i == '\n'){
-	count++;
+        sentence++;
     }
-    printf("\nThe numbers of strings in the sentence is %d", count);
+    printf("\nThe numbers of strings in the sentence is %d and the numbers of characters are %d", sentence, char1);
     
 }
